26e9110b83f51ab3a5b83c495d0ecef41ede2b47
osegonte <segohopo@gmail.com>
2025-09-19 12:47:12 +0200

    1

---
+++ apps/ios/lingua/lingua/linguaApp.swift
@@ -0,0 +1,17 @@
+//
+//  linguaApp.swift
+//  lingua
+//
+//  Created by osegonte hopo on 19.09.25.
+//
+
+import SwiftUI
+
+@main
+struct linguaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apps/ios/lingua/linguaTests/linguaTests.swift
@@ -0,0 +1,17 @@
+//
+//  linguaTests.swift
+//  linguaTests
+//
+//  Created by osegonte hopo on 19.09.25.
+//
+
+import Testing
+@testable import lingua
+
+struct linguaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
